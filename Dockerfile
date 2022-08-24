5af1c373ec9cd07f9adac1850a51fc83513b78e4
agustin rodriguez <agustinrodriguez206@gmail.com>
2022-08-24 01:47:56 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.9
+
+WORKDIR /app
+
+ENV PYTHONUNBUFFERED=1
+ENV PYTHONPYCACHEPREFIX=/tmp
+
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+
+ENV PYTHONUNBUFFERED=1
+CMD ["uvicorn", "src.main:app", "--reload", "--host", "0.0.0.0", "--port", "80"]
---
+++ src/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root(key: str ):
+
+    try:
+        url = "http://agrup.tplinkdns.com:8123/api/services/script/alarma_toggle_alarm"
+        token = key
+        payload = {}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}"
+        }
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+
+        return {"statusessaa": response.status_code}
+    except Exception as error:
+        return {"Error": error}
